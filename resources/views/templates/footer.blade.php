5a755db5c7fde98ae311a99371c497f18e73e30d
shalvinaza <shalvinazahwa@gmail.com>
2021-04-09 11:09:51 +0700

    register

---
+++ resources/views/templates/footer.blade.php
@@ -0,0 +1,37 @@
+<div class="container-fluid footerBbq">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-8">
+             <a style="font-family:Passion One; font-size:64px">
+                <img src="{{asset('assets/images/logodanta.png')}}" alt ="Logo" width=98px height=98px >
+                 BarbeQueue
+            </a>
+            <p style="padding:1.5%">One Stop Data Antrian Solution. Transform your business better. BarbeQueue simplify everything that looks so complex.</p>
+            </div>
+            <div class="col-sm-4" style="padding-left:3.5%">
+                <p style="font-size:24px">Contact Us</p>
+                <div class = "row" style="margin-bottom:3%">
+                    <a>
+                        <img src="{{asset('assets/images/email.png')}}" alt ="Email Icon" >
+                        barbequeue.company@barbe.ac.id
+                </a>
+                </div>
+                <div class = "row" style="margin-bottom:3%">
+                    <a >
+                        <img src="{{asset('assets/images/ig.png')}}" alt ="IG icon">
+                        Barbe.Queue
+                    </a>
+                </div>
+                <div class = "row" style="margin-bottom:3%">
+                    <a>
+                        <img src="{{asset('assets/images/fb.png')}}" alt ="FB icon">
+                        BarbeQueue Company
+                    </a>
+                </div>
+            </div>
+            <div class="copyright">
+            <p style="margin-top:24px">&copy 2021 BarbeQueue | Danta’s Angels . All Right Reserved</p>    
+            </div>
+        </div>
+    </div>
+</div>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('halo', function () {
+    return "halo shal";
+});
+Route::get('register','Register@index');
