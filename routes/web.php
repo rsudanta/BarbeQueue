6bd0aeae18261b41f300fadc3e90490c96b39920
Bunga Azizha <bungaazizha33@gmail.com>
2021-04-12 18:28:56 +0700

    login

--- routes/web.php
+++ routes/web.php
@@ -16,14 +16,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-<<<<<<< HEAD
 
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-=======
 Route::get('halo', function () {
     return "halo shal";
 });
 Route::get('register','Register@index');
->>>>>>> 5a755db5c7fde98ae311a99371c497f18e73e30d
+Route::get('login','Login@index');
