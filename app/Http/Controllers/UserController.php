c56a7b79538e80d998cc2a1a8bb68d08d0e7e5e0
Danta <rsudanta20@gmail.com>
2021-05-25 11:00:01 +0700

    fixed my activity

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -43,8 +43,14 @@ class UserController extends Controller
     }
     public function aktivitas()
     {
-        $ongoing = Antrian::where('user_id', Auth::id())->where('status', 'On Going')->orWhere('status', 'Waiting')->get();
-        $done = Antrian::where('user_id', Auth::id())->where('status', 'Done')->orWhere('status', 'Expired')->get();
+        $ongoing = Antrian::where('user_id', Auth::id())->where(function ($q) {
+            $q->where('status', 'On Going')
+                ->orWhere('status', 'Waiting');
+        })->get();
+        $done = Antrian::where('user_id', Auth::id())->where(function ($q) {
+            $q->where('status', 'Done')
+                ->orWhere('status', 'Expired');
+        })->get();
         return view('v_aktivitas', [
             'ongoing' => $ongoing,
             'done' => $done
@@ -83,27 +89,27 @@ class UserController extends Controller
     }
 
 
-    public function changePassword(Request $request){
+    public function changePassword(Request $request)
+    {
 
         if (!(Hash::check($request->get('current-password'), Auth::user()->password))) {
-        // The passwords matches
-        return redirect()->back()->with("error","Your current password does not matches with the password you provided. Please try again.");
+            // The passwords matches
+            return redirect()->back()->with("error", "Your current password does not matches with the password you provided. Please try again.");
         }
-         
-        if(strcmp($request->get('current-password'), $request->get('new-password')) == 0){
-        //Current password and new password are same
-        return redirect()->back()->with("error","New Password cannot be same as your current password. Please choose a different password.");
+
+        if (strcmp($request->get('current-password'), $request->get('new-password')) == 0) {
+            //Current password and new password are same
+            return redirect()->back()->with("error", "New Password cannot be same as your current password. Please choose a different password.");
         }
-        if(!(strcmp($request->get('new-password'), $request->get('new-password-confirm'))) == 0){
-                    //New password and confirm password are not same
-                    return redirect()->back()->with("error","New Password should be same as your confirmed password. Please retype new password.");
+        if (!(strcmp($request->get('new-password'), $request->get('new-password-confirm'))) == 0) {
+            //New password and confirm password are not same
+            return redirect()->back()->with("error", "New Password should be same as your confirmed password. Please retype new password.");
         }
         //Change Password
         $user = Auth::user();
         $user->password = bcrypt($request->get('new-password'));
         $user->save();
-         
+
         return redirect()->route('user_profile');
-         
-        }
+    }
 }
